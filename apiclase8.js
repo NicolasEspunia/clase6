dc441ccfca177066a0da7c937b2ac2edcf3d3b49
NICOLAS <nicolas_espunia@hotmail.com>
2021-10-02 20:05:27 -0300

    api

--- apiclase8.js
+++ apiclase8.js
@@ -1,17 +1,43 @@
-const API_URL = "https://jsonplaceholder.typicode.com/";
 
-const xhr = new XMLHttpRequest();
 
-function onRequestHandler(){
-    if(this.readyState === 4 && this.status===200){
-        //0=unset
-        //1 = opened
-        //2=header received
-        //3 =lodaing
-        //4=done
-        console.log(this.response);
-    }
-}
-xhr.addEventListener("load",onRequestHandler);
-xhr.open("GET",`${API_URL}/users`);
-xhr.send();
+const API_URL = "https://jsonplaceholder.typicode.com/users/1";
+
+// const xhr = new XMLHttpRequest();
+
+// function onRequestHandler(){
+//     if(this.readyState === 4 && this.status === 200){
+//         //0=unset
+//         //1 = opened
+//         //2=header received
+//         //3 =lodaing
+//         //4=done
+//         const data = JSON.parse(this.response);
+//         const HTMLResponse = document.querySelector("#app");
+
+//         const tpl = data.map(user => `<li>${user.name}  ,${user.email}</li>`)
+//     HTMLResponse.innerHTML =`<ul>${tpl}</ul>`;
+//     }
+
+// }
+// xhr.addEventListener("load",onRequestHandler);
+// xhr.open('GET',`${API_URL}/users`);
+// xhr.send();
+
+//  API DE FETCH
+
+
+const HTMLResponse = document.querySelector("#app");
+const ul = document.createElement('ul');
+
+fetch(` ${API_URL}/users`)
+.then((response)=>response.json())
+.then((users)=>{
+    users.forEach((user)=>{
+        let elem = document.createElement("li");
+        elem.appendChild(
+            document.createTextNode(`${user.name} ${user.email}`)
+        );
+        ul.appendChild(elem);
+    });
+    HTMLResponse.appendChild(ul);
+})
